45debb90d6ce37d90b5adebcbf08148c510f2545
Amit Bhatt <amitbhatt54341@gmail.com>
2026-01-11 18:53:44 +0530

    final changes

--- reactapp/src/apiConfig.js
+++ reactapp/src/apiConfig.js
@@ -1,5 +1,7 @@
 // src/apiConfig.js
-const baseUrl = import.meta.env.VITE_API_BASE_URL;
+//const baseUrl = import.meta.env.VITE_API_BASE_URL;
+const baseUrl = process.env.REACT_APP_API_BASE_URL; 
+
 if (!baseUrl) {
   console.error("❌ VITE_API_BASE_URL is undefined");
 }
